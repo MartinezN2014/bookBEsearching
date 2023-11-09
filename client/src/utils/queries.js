3f50fa4cbd7b8be18cb55b0f139e4c32f2663908
MartinezN2014 <2014nmartinez@gmail.com>
2023-11-08 22:22:13 -0500

    forgot to import my quaries and mutations

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,17 +1,18 @@
 import { gql } from "@apollo/client";
 
-export const GET_ME = gql`
+export const QUERY_ME = gql`
   {
     me {
       _id
       username
       email
+      bookCount
       savedBooks {
         bookId
         authors
-        image
-        description
         title
+        description
+        image
         link
       }
     }
